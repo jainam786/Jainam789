d3b7633387e1226289482ad7f0e8e14155ede736
jainam786 <bdoshi700@gmail.com>
2024-08-28 09:57:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7535053047:AAHupO2NTpXijo3Uz7sYxq3Nmsqlw-ChQBA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("jainam2612")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5006333904")
+
+
+
+
+
+
+
+  
